d1a3a3ea6684d0cb4fac3f820df0965f2abdcf26
Gueru <rubencontrerasguerrero@hotmail.com>
2025-02-17 10:29:37 -0500

    feat: Download view updated, and SVGs fixed

--- apps/resume-web/src/routes/pdf/index.tsx
+++ apps/resume-web/src/routes/pdf/index.tsx
@@ -2,6 +2,7 @@ import { use, useEffect, useRef, useState } from 'react'
 import { Button } from '@resume/ui'
 import { createFileRoute } from '@tanstack/react-router'
 import { CacheContext } from '@providers/globalCache'
+import { Download } from '@resume/icons'
 
 const worker = new Worker(new URL('./-src/worker.ts', import.meta.url), { type: 'module' })
 
@@ -43,9 +44,11 @@ function RouteComponent() {
     })
   }
 
-  return <div className='relative left-16'>
-    <Button disabled={!hasFinished} onClick={handleClick} themeColor='primary' variant='solid'>
-      Create pdf
+  return <div className='relative left-16 flex flex-col justify-center h-lvh w-[80%] max-w-[360px] gap-3'>
+    <h2 className='font-bold text-4xl'>Congratulations!</h2>
+    <p className='text-justify'>Your resume is awesome ready for download. Get it in any format of your choice.</p>
+    <Button disabled={!hasFinished} onClick={handleClick} themeColor='primary' variant='solid' endIcon={<Download />} className='flex justify-center'>
+      Download
     </Button>
     <a ref={anchorRef} hidden download='resume.pdf' />
   </div>
