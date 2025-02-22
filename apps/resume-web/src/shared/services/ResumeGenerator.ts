bcd7e53f0ab283bee2888fddaf5a69788458367b
Paul Chavez <paulchavezromero20@gmail.com>
2025-02-21 19:05:12 -0500

    feat: update resume generator to use 'finishDate' and improve formatting

--- apps/resume-web/src/shared/services/ResumeGenerator.ts
+++ apps/resume-web/src/shared/services/ResumeGenerator.ts
@@ -14,14 +14,14 @@ export interface ResumeGeneratorData {
     school: string;
     degree: string;
     startDate: string;
-    endDate: string;
-  }[],
+    finishDate: string;
+  },
   [CacheKeys.Experience]?: {
     company: string;
     position: string;
     startDate: string;
-    endDate: string;
-  }[],
+    finishDate: string;
+  },
   [CacheKeys.TechnicalSkills]?: {
     skillList: string[];
   },
@@ -47,87 +47,86 @@ export class ResumeGenerator {
     const educations = [this.data[CacheKeys.Education]]
     const experiences = [this.data[CacheKeys.Experience]]
 
-  console.log({educations, experiences})
     const doc = new Document({
       sections: [
-      {
-        children: [
-        new Paragraph({
-          heading: HeadingLevel.HEADING_1,
-          alignment: AlignmentType.CENTER,
+        {
           children: [
-          new TextRun({
-            text: this.data[CacheKeys.PersonalInfo]?.name,
-            bold: true,
-            size: 32,
-            color: '000000'
-          }),
-          ]
-        }),
-        new Paragraph({
-          children: [
-          new TextRun(this.data[CacheKeys.PersonalInfo]?.city ?? ''),
-          new TextRun(', '),
-          new TextRun(this.data[CacheKeys.PersonalInfo]?.state ?? ''),
-          new TextRun(' | '),
-          new TextRun(this.data[CacheKeys.ContactInfo]?.phoneNumber ?? ''),
-          new TextRun(' | '),
-          new TextRun(this.data[CacheKeys.ContactInfo]?.linkedInProfile ?? ''),
-          new TextRun(' | '),
-          new TextRun(this.data[CacheKeys.PersonalInfo]?.email ?? ''),
+            new Paragraph({
+              heading: HeadingLevel.HEADING_1,
+              alignment: AlignmentType.CENTER,
+              children: [
+                new TextRun({
+                  text: this.data[CacheKeys.PersonalInfo]?.name,
+                  bold: true,
+                  size: 32,
+                  color: '000000'
+                }),
+              ]
+            }),
+            new Paragraph({
+              children: [
+                new TextRun(this.data[CacheKeys.PersonalInfo]?.city ?? ''),
+                new TextRun(', '),
+                new TextRun(this.data[CacheKeys.PersonalInfo]?.state ?? ''),
+                new TextRun(' | '),
+                new TextRun(this.data[CacheKeys.ContactInfo]?.phoneNumber ?? ''),
+                new TextRun(' | '),
+                new TextRun(this.data[CacheKeys.ContactInfo]?.linkedInProfile ?? ''),
+                new TextRun(' | '),
+                new TextRun(this.data[CacheKeys.PersonalInfo]?.email ?? ''),
+              ],
+              alignment: AlignmentType.CENTER
+            }),
+            new Paragraph({
+              heading: HeadingLevel.HEADING_2,
+              children: [
+                new TextRun('Skills'),
+              ]
+            }),
+            new Paragraph({
+              children: [
+                new TextRun({
+                  text: this.data[CacheKeys.TechnicalSkills]?.skillList.join(', '),
+                }),
+              ]
+            }),
+            new Paragraph({
+              heading: HeadingLevel.HEADING_2,
+              children: [
+                new TextRun('Education')
+              ]
+            }),
+            ...(educations?.map(education => new Paragraph({
+              children: [
+                new TextRun(`${education?.school}, ${education?.degree} (${education?.startDate} - ${education?.finishDate})`)
+              ]
+            })) ?? []),
+            new Paragraph({
+              heading: HeadingLevel.HEADING_2,
+              children: [
+                new TextRun('Experience')
+              ]
+            }),
+            ...(experiences?.map(experience => new Paragraph({
+              children: [
+                new TextRun(`${experience?.company}, ${experience?.position} (${experience?.startDate} - ${experience?.finishDate})`)
+              ]
+            })) ?? []),
+            new Paragraph({
+              heading: HeadingLevel.HEADING_2,
+              children: [
+                new TextRun('Certification'),
+              ]
+            }),
+            new Paragraph({
+              children: [
+                new TextRun({
+                  text: this.data[CacheKeys.Certification]?.certifications.join(', ')
+                })
+              ]
+            })
           ],
-          alignment: AlignmentType.CENTER
-        }),
-        new Paragraph({
-          heading: HeadingLevel.HEADING_2,
-          children: [
-          new TextRun('Skills'),
-          ]
-        }),
-        new Paragraph({
-          children: [
-          new TextRun({
-            text: this.data[CacheKeys.TechnicalSkills]?.skillList.join(', '),
-          }),
-          ]
-        }),
-        new Paragraph({
-          heading: HeadingLevel.HEADING_2,
-          children: [
-          new TextRun('Education')
-          ]
-        }),
-        ...(educations?.map(education => new Paragraph({
-          children: [
-          new TextRun(`${education?.school}, ${education?.degree} (${education?.startDate} - ${education?.endDate })`)
-          ]
-        })) ?? []),
-        new Paragraph({
-          heading: HeadingLevel.HEADING_2,
-          children: [
-          new TextRun('Experience')
-          ]
-        }),
-        ...(experiences?.map(experience => new Paragraph({
-          children: [
-          new TextRun(`${experience.company}, ${experience.position} (${experience.startDate} - ${experience.endDate})`)
-          ]
-        })) ?? []),
-        new Paragraph({
-          heading: HeadingLevel.HEADING_2,
-          children: [
-          new TextRun('Certification'),
-          ]
-        }),
-        new Paragraph({
-          children: [
-          new TextRun({
-            text: this.data[CacheKeys.Certification]?.certifications.join(', ')
-          })
-          ]
-        })
-        ],
-      },
+        },
       ]
     })
 
